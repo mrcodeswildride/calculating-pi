ba30a1993e34ccd43c1e88a7bf4f535097917555
David Keller <kellerdb@gmail.com>
2021-05-16 08:34:43 -0700

    formatting

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let number = document.getElementById(`number`)
 let piButton = document.getElementById(`piButton`)
-let box = document.getElementById(`box`)
+let table = document.getElementById(`table`)
 
 piButton.addEventListener(`click`, calculatePi)
 
@@ -10,46 +10,35 @@ number.focus()
 function calculatePi() {
   let numberValue = number.value.trim()
 
-  if (numberValue != `` && !isNaN(numberValue)) {
-    if (numberValue < 6 || numberValue > 100663296) {
-      box.innerHTML = `Number of sides must be between 6 and 100,663,296.`
+  if (numberValue < 6 || numberValue > 100663296) {
+    table.innerHTML = `Number of sides must be between 6 and 100,663,296.`
+  } else {
+    let sides = []
+    sides[6] = 1
+
+    for (let i = 12; i <= numberValue; i *= 2) {
+      let a = Math.sqrt(1 - Math.pow(sides[i / 2] / 2, 2))
+      let b = 1 - a
+
+      sides[i] = Math.sqrt(Math.pow(b, 2) + Math.pow(sides[i / 2] / 2, 2))
     }
-    else {
-      let sides = []
-      sides[6] = 1
 
-      for (let i = 12; i <= numberValue; i *= 2) {
-        let a = Math.sqrt(1 - Math.pow(sides[i/2] / 2, 2))
-        let b = 1 - a
+    table.innerHTML = ``
+    makeRow(`# of Sides`, `<strong>Pi Approximation</strong>`)
 
-        sides[i] = Math.sqrt(Math.pow(b, 2) + Math.pow(sides[i/2] / 2, 2))
-      }
+    for (let i = 6; i <= numberValue; i *= 2) {
+      let c = i * sides[i]
+      let pi = c / 2
 
-      box.innerHTML = ``
-      createRow(`# of Sides`, `<strong>Pi Approximation</strong>`)
-
-      for (let i = 6; i <= numberValue; i *= 2) {
-        let c = i * sides[i]
-        let pi = c / 2
-
-        createRow(i, pi)
-      }
+      makeRow(i, pi)
     }
-  }
-
-  number.focus()
-}
-
-function keyPressed(event) {
-  if (event.keyCode == 13) {
-    calculatePi()
   }
 }
 
-function createRow(key, value) {
+function makeRow(key, value) {
   let row = document.createElement(`div`)
   row.classList.add(`row`)
-  box.appendChild(row)
+  table.appendChild(row)
 
   let keyDiv = document.createElement(`div`)
   keyDiv.classList.add(`key`)
@@ -60,4 +49,10 @@ function createRow(key, value) {
   valueDiv.classList.add(`value`)
   valueDiv.innerHTML = value
   row.appendChild(valueDiv)
+}
+
+function keyPressed(event) {
+  if (event.keyCode == 13) {
+    calculatePi()
+  }
 }
